d204641756746df79a5cca15f811c6196a74ac6f
dm7111722 <dm7111733@gmail.com>
2015-11-25 12:18:50 +0800

    Calculate
    calculations galore

---
+++ DMCalculate.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "Windows.h"
+
+using namespace std;
+
+
+
+void Addition()
+{
+	for(int i=0; i<9999999; i++)
+	{
+		cin>>number1;
+		cin>>number2;
+		
+		number1 + number2;
+		
+		cout<<number1 + number2<<endl;
+		
+	}
+}
+
+void Subtraction
+{
+		for(int i=0; i<9999999; i++)
+	{
+		cin>>number1;
+		cin>>number2;
+		
+		number1 - number2;
+		
+		cout<<number1 - number2<<endl;
+		
+	}
+}
