ec72b0284aa63f0407887fb5f28675ff1fc12b98
BLim <addidas_keng995@hotmail.com>
2015-11-25 12:16:28 +0800

    Number Script

---
+++ WhatHappensWithFolders/Number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+
+int num;
+
+int main()
+{
+	cout<<"Type a number!" <<endl;
+	cin>>num;
+	cout<<endl;
+	cout<<num<<" is AWESOME! Woo!"<<endl;
+	
+	return 0;
+}
